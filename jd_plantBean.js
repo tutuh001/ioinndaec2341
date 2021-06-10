8067577596c2f787db757c6cc60059c78e59a310
tutuh001 <75411328+tutuh001@users.noreply.github.com>
2021-06-10 11:08:25 +0800

    Update jd_plantBean.js

--- jd_plantBean.js
+++ jd_plantBean.js
@@ -34,7 +34,7 @@ const JD_API_HOST = 'https://api.m.jd.com/client.action';
 //此此内容是IOS用户下载脚本到本地使用，填写互助码的地方，同一京东账号的好友互助码请使用@符号隔开。
 //下面给出两个账号的填写示例（iOS只支持2个京东账号）
 let shareCodes = [ // IOS本地脚本用户这个列表填入你要助力的好友的shareCode
-                    '4b4k7ivqtsj5m7erbrz5gflnuq@3lzxilsa4g5kxadyan666vugy45ac3f4ijdgqji@mjwdhb3s3vwjxw4bzaxvqs3kkq@wsr6thb5bd25kudjbuw6shngkrctibwwbaskxma@suqg5cye47cqnsqfvx3sctidcauk4jrajpuyh4y@grvrcqr6cmg4xr3e56lzvgml5a3h7wlwy7o5jii',//账号一的好友shareCode,不同好友中间用@符号隔开
+   '4b4k7ivqtsj5m7erbrz5gflnuq@3lzxilsa4g5kxadyan666vugy45ac3f4ijdgqji@mjwdhb3s3vwjxw4bzaxvqs3kkq@wsr6thb5bd25kudjbuw6shngkrctibwwbaskxma@suqg5cye47cqnsqfvx3sctidcauk4jrajpuyh4y@grvrcqr6cmg4xr3e56lzvgml5a3h7wlwy7o5jii',//账号一的好友shareCode,不同好友中间用@符号隔开
   '4b4k7ivqtsj5m7erbrz5gflnuq@3lzxilsa4g5kxadyan666vugy45ac3f4ijdgqji@onw2qibszezspqpj7cfkcy3vqu3h7wlwy7o5jii@wsr6thb5bd25kudjbuw6shngkrctibwwbaskxma@suqg5cye47cqnsqfvx3sctidcauk4jrajpuyh4y@grvrcqr6cmg4xr3e56lzvgml5a3h7wlwy7o5jii',//账号二的好友shareCode，不同好友中间用@符号隔开
   '4b4k7ivqtsj5m7erbrz5gflnuq@3lzxilsa4g5kxadyan666vugy45ac3f4ijdgqji@onw2qibszezspqpj7cfkcy3vqu3h7wlwy7o5jii@mjwdhb3s3vwjxw4bzaxvqs3kkq@suqg5cye47cqnsqfvx3sctidcauk4jrajpuyh4y@grvrcqr6cmg4xr3e56lzvgml5a3h7wlwy7o5jii',//账号三的好友shareCode，不同好友中间用@符号隔开
   '4b4k7ivqtsj5m7erbrz5gflnuq@3lzxilsa4g5kxadyan666vugy45ac3f4ijdgqji@onw2qibszezspqpj7cfkcy3vqu3h7wlwy7o5jii@mjwdhb3s3vwjxw4bzaxvqs3kkq@wsr6thb5bd25kudjbuw6shngkrctibwwbaskxma@grvrcqr6cmg4xr3e56lzvgml5a3h7wlwy7o5jii',//账号四的好友shareCode，不同好友中间用@符号隔开
