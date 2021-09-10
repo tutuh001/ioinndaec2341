305139cbac34c11d4ac64484aeca8408e5ad8af0
tutuh001 <75411328+tutuh001@users.noreply.github.com>
2021-09-10 14:50:32 +0800

    Update jd_jdzz.js

--- jd_jdzz.js
+++ jd_jdzz.js
@@ -39,7 +39,11 @@ if ($.isNode()) {
   cookiesArr = [$.getdata('CookieJD'), $.getdata('CookieJD2'), ...jsonParse($.getdata('CookiesJD') || "[]").map(item => item.cookie)].filter(item => !!item);
 }
 const JD_API_HOST = 'https://api.m.jd.com/client.action';
-const inviteCodes = ['']
+const inviteCodes = ['S96wuFVpM8lPQ',
+                    'S96wuFVpM8lPQ',
+                    'S96wuFVpM8lPQ',
+                    'S96wuFVpM8lPQ',
+                    'S96wuFVpM8lPQ',]
 let nowTimes = new Date(new Date().getTime() + new Date().getTimezoneOffset() * 60 * 1000 + 8 * 60 * 60 * 1000);
 !(async () => {
   await requireConfig();
